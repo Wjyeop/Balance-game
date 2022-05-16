51d418a981f6c94cc1cb8d248538d7c8464cce29
WON JUNYEOP <wnsduq2178@gmail.com>
2022-05-16 15:10:57 +0900

    기초 세팅

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+
+    return(
+        <div>
+            안녕하세여 우하하
+        </div>
+    )
+
+}
+
+export default Footer
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+
+    return(
+        <div>
+            안녕하세여 우하하
+        </div>
+    )
+
+}
+
+export default Header
